aa3b63e58987537a0f38ae7143eecdccbad9df5d
Dmitry Sergeev <jedi.n081@gmail.com>
2018-02-06 01:47:21 +0400

    comments

--- src/components/Tasks/reducer.js
+++ src/components/Tasks/reducer.js
@@ -49,21 +49,21 @@ export default function tasksReducer(state = initialState, action) {
 
     case COMPLETE_TASK:
       return  {
-        ...state,
-        items: state.items.map((item, index) => {
-          if (item.id == (action.index + 1)) {
-            return {
-              ...item,
-              checked: action.checked,
-              filters: item.filters.map((filter, index) => {
-                if (index === 0) {
-                  return { ...filter, filter: action.checked }
+        ...state, // берём наш стейт..
+        items: state.items.map((item, index) => { // маппим его..
+          if (item.id == (action.index + 1)) { // ищем совпадение по  id..
+            return { // если нашёл такой объект, то..
+              ...item, // берём этот объект..
+              checked: action.checked, // если нужно, что-нибудь меняем в нём по поути..
+              filters: item.filters.map((filter, index) => { // затем берём массив фильтров и маппим его..
+                if (index === 0) { // тут нужно будет придумать, что мы будем сравнивать и с чем, чтобы опознать наш объект..
+                  return { ...filter, filter: action.checked } // если нашёлся наш фильтр, меняем его. У вас как раз будет checked: true/false
                 }
-                return filter;
+                return filter; // если не совпало, то возврашаем фильтр, и так на каждой итерации, пока не найдём нужный
               }) 
             } 
           }
-          return item;
+          return item; // если не совпало, то возврашаем наш итем, и так на каждой итерации, пока не найдём нужный
         }
       )
       };
